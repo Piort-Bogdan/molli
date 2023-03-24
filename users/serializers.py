e529480240b07e1cbfafa71932fb6dda267940c0
Piort-Bogdan <piort.bogdan@gmail.com>
2023-03-24 23:54:45 +0200

    Add unit tests

--- users/serializers.py
+++ users/serializers.py
@@ -6,13 +6,13 @@ from .models import Pet, User
 class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
-        fields = '__all__'
+        fields = ('name', 'year_of_birth', 'species', 'owner')
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'password', 'username', 'phone_number', 'address', 'name', 'job_title', 'email', )
 
     def create(self, validated_data):
         user = super().create(validated_data)
@@ -20,3 +20,5 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(user.password)
         user.save()
         return user
+
+
--- users/tests/test_serializers.py
+++ users/tests/test_serializers.py
@@ -1,36 +1,62 @@
-from django.test   import TestCase
+from django.test import TestCase
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Pet
+from users.serializers import UserSerializer, PetSerializer
 
 
 class UserSerializerTestCase(TestCase):
+    def setUp(self):
+        self.user_1 = User.objects.create(password='1232424', username='user1', phone_number='12312312312',
+                                          address='test address1', name='full name test1', job_title='DOC',
+                                          email='emailtest1@mail.ru', )
+        self.user_2 = User.objects.create(password='12324', username='user2', phone_number='1231312',
+                                          address='test address2', name='full name test2', job_title='DOC2',
+                                          email='emailtest2@mail.ru', )
+        self.pet_1 = Pet.objects.create(name='test_pet_name1', year_of_birth='1997-09-09',
+                                        species='dog', owner=self.user_1)
+        self.pet_2 = Pet.objects.create(name='test_pet_name2', year_of_birth='1996-09-09',
+                                        species='dog2', owner=self.user_2)
 
     def test_user_serializer(self):
-        self.user_1 = User.objects.create(password='1232424', username='user1', phone_number='12312312312', address='test address1',
-                                          name='full name test1',  job_title='DOC')
-        self.user_2 = User.objects.create(username='user2', phone_number='8767', address='test address2',
-                                          name='full name test2', password='123wasd24', job_title= 'DOC2')
-
         serialized_data = UserSerializer([self.user_1, self.user_2], many=True).data
 
         expected_data = [{
             'id': self.user_1.id,
             'password': '1232424',
             'username': 'user1',
-            'job_title': 'DOC',
-            'name': 'full name test1',
             'phone_number': '12312312312',
-            'address': 'test address1'
+            'address': 'test address1',
+            'name': 'full name test1',
+            'job_title': 'DOC',
+            'email': 'emailtest1@mail.ru',
         },
-        {
-            'id': self.user_2.id,
-            'password': '123wasd24',
-            'username': 'user2',
-            'job_title': 'DOC2',
-            'name': 'full name test2',
-            'phone_number': '8767',
-            'address': 'test address2',
-        }]
+            {
+                'id': self.user_2.id,
+                'password': '12324',
+                'username': 'user2',
+                'phone_number': '1231312',
+                'address': 'test address2',
+                'name': 'full name test2',
+                'job_title': 'DOC2',
+                'email': 'emailtest2@mail.ru',
+            }]
 
         self.assertEqual(expected_data, serialized_data, serialized_data)
+
+    def test_pet_serializer(self):
+        serializers_data = PetSerializer([self.pet_1, self.pet_2], many=True).data
+
+        expected_data = [{
+            'name': 'test_pet_name1',
+            'year_of_birth': '1997-09-09',
+            'species': 'dog',
+            'owner': self.user_1.id
+        },
+            {
+                'name': 'test_pet_name2',
+                'year_of_birth': '1996-09-09',
+                'species': 'dog2',
+                'owner': self.user_2.id
+            }]
+
+        self.assertEqual(serializers_data, expected_data, (serializers_data, expected_data))
